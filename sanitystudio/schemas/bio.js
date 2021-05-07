a8a75ac6102771341144130e8391c52639e0cb67
Stephan Loh <sloh4137@gmail.com>
2021-05-07 00:16:18 -0400

    Add initial bio schema

---
+++ sanitystudio/schemas/bio.js
@@ -0,0 +1,17 @@
+export default {
+    name: 'bio',
+    title: 'Bio',
+    type: 'document',
+    fields: [
+        {
+            name: 'name',
+            title: 'Name',
+            type: 'string',
+        },
+        {
+            name: 'position',
+            title: 'Position',
+            type: 'string',
+        },
+    ],
+}
--- website/gatsby-config.js
+++ website/gatsby-config.js
@@ -25,5 +25,21 @@ module.exports = {
     },
     "gatsby-plugin-typescript",
     "gatsby-plugin-typegen",
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+          name: "Organization Website",
+          short_name: "Org Website",
+          start_url: "/",
+          background_color: "#663399",
+          theme_color: "#663399",
+          display: "minimal-ui",
+          icon: "static/logo.png", // This path is relative to the root of the site.
+      },
+  },
+
+  // this (optional) plugin enables Progressive Web App + Offline functionality
+  // To learn more, visit: https://gatsby.dev/offline
+  "gatsby-plugin-offline",
   ],
 };
