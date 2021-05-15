2f00bbeeceb698491c60b2c3d1a11c1d8d4fc497
Stephan Loh <sloh4137@gmail.com>
2021-05-14 23:26:38 -0400

    Convert everything to Typescript

--- sanitystudio/lib/isUniqueAcrossAllDocuments.ts
+++ sanitystudio/lib/isUniqueAcrossAllDocuments.ts
@@ -1,8 +1,13 @@
 import client from "part:@sanity/base/client"
+import { SanityDocument } from "@sanity/types"
+
+type Options = {
+    document: SanityDocument
+}
 
 // Note: this assumes that every document that has a slug field
 // have it on the `slug` field at the root
-export function isUniqueAcrossAllDocuments(slug, options) {
+export function isUniqueAcrossAllDocuments(slug: string, options: Options) {
     const { document } = options
 
     const id = document._id.replace(/^drafts\./, "")
