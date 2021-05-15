f6ea20b3d2a79d6bfe2171d1ae746f1b323bd1d0
Stephan Loh <sloh4137@gmail.com>
2021-05-14 23:37:36 -0400

    Add validation to bio schema

--- sanitystudio/schemas/bio.ts
+++ sanitystudio/schemas/bio.ts
@@ -7,11 +7,13 @@ export default {
             name: "name",
             title: "Name",
             type: "string",
+            validation: (Rule) => Rule.required(),
         },
         {
             name: "position",
             title: "Position",
             type: "string",
+            validation: (Rule) => Rule.required(),
         },
         {
             name: "boardYear",
@@ -19,6 +21,12 @@ export default {
             type: "string",
             description:
                 "The year the person was on board. For example: 2019-2020",
+            validation: (Rule) =>
+                Rule.required()
+                    .regex(/\d{4}\d{4}/, {
+                        name: "board year"
+                    })
+                    .error("Years should be in the format XXXX-XXXX"),
         },
         {
             name: "major",
