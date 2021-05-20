c0c037b1c99328706941f45e112287a41b32ec7f
Stephan Loh <sloh4137@gmail.com>
2021-05-19 22:13:17 -0400

    Add browser and ssr files. Update config with alias

--- website/gatsby-config.js
+++ website/gatsby-config.js
@@ -1,45 +1,53 @@
 module.exports = {
-  siteMetadata: {
-    title: "Organization Website",
-  },
-  plugins: [
-    {
-      resolve: "gatsby-source-sanity",
-      options: {
-        projectId: "tbo3siwk",
-        dataset: "production",
-      },
+    siteMetadata: {
+        title: "Organization Website",
     },
-    "gatsby-plugin-gatsby-cloud",
-    "gatsby-plugin-image",
-    "gatsby-plugin-react-helmet",
-    "gatsby-plugin-sharp",
-    "gatsby-transformer-sharp",
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        name: "images",
-        path: "./src/images/",
-      },
-      __key: "images",
-    },
-    "gatsby-plugin-typescript",
-    "gatsby-plugin-typegen",
-    {
-      resolve: "gatsby-plugin-manifest",
-      options: {
-          name: "Organization Website",
-          short_name: "Org Website",
-          start_url: "/",
-          background_color: "#663399",
-          theme_color: "#663399",
-          display: "minimal-ui",
-          icon: "static/logo.png", // This path is relative to the root of the site.
-      },
-  },
+    plugins: [
+        {
+            resolve: "gatsby-source-sanity",
+            options: {
+                projectId: "tbo3siwk",
+                dataset: "production",
+            },
+        },
+        "gatsby-plugin-gatsby-cloud",
+        "gatsby-plugin-image",
+        "gatsby-plugin-react-helmet",
+        "gatsby-plugin-sharp",
+        "gatsby-transformer-sharp",
+        {
+            resolve: "gatsby-source-filesystem",
+            options: {
+                name: "images",
+                path: "./src/images/",
+            },
+            __key: "images",
+        },
+        {
+            resolve: `gatsby-alias-imports`,
+            options: {
+                aliases: {
+                    root: `/`,
+                },
+            },
+        },
+        "gatsby-plugin-typescript",
+        "gatsby-plugin-typegen",
+        {
+            resolve: "gatsby-plugin-manifest",
+            options: {
+                name: "Organization Website",
+                short_name: "Org Website",
+                start_url: "/",
+                background_color: "#663399",
+                theme_color: "#663399",
+                display: "minimal-ui",
+                icon: "static/logo.png", // This path is relative to the root of the site.
+            },
+        },
 
-  // this (optional) plugin enables Progressive Web App + Offline functionality
-  // To learn more, visit: https://gatsby.dev/offline
-  "gatsby-plugin-offline",
-  ],
-};
+        // this (optional) plugin enables Progressive Web App + Offline functionality
+        // To learn more, visit: https://gatsby.dev/offline
+        "gatsby-plugin-offline",
+    ],
+}
