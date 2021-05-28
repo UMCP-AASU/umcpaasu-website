7056b30e7130e501e3fdaf87cb431755c8055215
Stephan Loh <sloh4137@gmail.com>
2021-05-28 15:13:48 -0400

    Add initial Sanity Content component

---
+++ website/src/components/SanityContent/SanityContent.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+import BlockContent from "@sanity/block-content-to-react"
+
+type Props = {
+
+}
+
+const SanityContent = ({}: Props) => {
+
+}
+
+export default SanityContent
