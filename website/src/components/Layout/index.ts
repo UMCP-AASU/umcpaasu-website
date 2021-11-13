158f1f9842821b9a4a98112b3584cbe0320ea214
Stephan Loh <sloh4137@gmail.com>
2021-11-12 20:40:14 -0800

    Add layout components

--- website/src/components/Layout/index.ts
+++ website/src/components/Layout/index.ts
@@ -1,3 +1,5 @@
 export { default as ParallaxBackground } from "./ParallaxBackground"
 export { default as RaisedPageContent } from "./RaisedPageContent"
 export { default as Section } from "./Section"
+export { default as GridWithItems } from "./GridWithItems"
+export { default as AnimateOnVisible } from "./AnimateOnVisible"
