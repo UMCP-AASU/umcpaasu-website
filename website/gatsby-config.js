a5d2127a10589105136ce211431282f852d37d74
Stephan Loh <sloh@fb.com>
2021-11-28 21:10:33 -0500

    Remove gatsby-plugin-offline

--- website/gatsby-config.js
+++ website/gatsby-config.js
@@ -40,6 +40,10 @@ module.exports = {
 
         // this (optional) plugin enables Progressive Web App + Offline functionality
         // To learn more, visit: https://gatsby.dev/offline
-        "gatsby-plugin-offline",
+        // "gatsby-plugin-offline",
+        // Removing gatsby-plugin-offline because it's causing issues with caching and I'm
+        // too lazy to figure out how to fix that.
+        // In order to safely remove, we also have to replace it with gatsby-plugin-remove-serviceworker
+        `gatsby-plugin-remove-serviceworker`,
     ],
 }
